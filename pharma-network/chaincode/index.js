a933781174332b5c44f8583a980ee2160cbd6802
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-01 12:59:17 +0800

    Delete pharma-network directory

--- pharma-network/chaincode/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const registrationcontract = require("./RegistrationContract.js");
-const drugRegistrationcontract = require("./DrugRegistration.js");
-const transferDrugContract = require("./TransferDrug.js");
-const viewLifecycleContract = require("./ViewLifeCycle.js");
-
-module.exports.contracts = [registrationcontract, drugRegistrationcontract, transferDrugContract, viewLifecycleContract];
